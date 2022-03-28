c311d356a9dc6b3ecf3a7b89ec66ca7bf6de63a4
Jaime Rios <jaimerios1989@gmail.com>
2022-03-27 20:05:15 -0700

    basic node server

--- _practice/node/node-server.js
+++ _practice/node/node-server.js
@@ -4,13 +4,13 @@ function sitioWeb(req, res){
     // res.writeHead(200, {"Content-type": "text/plain"})
     // res.end("Primer Servidor...")
     res.writeHead(200, {"Content-type": "text/html"})
-    res.end("<b>Segundo</b> Servidor...")
+    res.end("<b>Primer</b> Servidor...")
 }
 
 let servidor = http.createServer(sitioWeb)
 
-// servidor.listen(420, "192.168.0.14") // ip local
-servidor.listen(420, "127.0.0.1")
+servidor.listen(420, "192.168.0.18") // ip local
+// servidor.listen(420, "127.0.0.1")
 
 console.log("Node Server corriendo...") 
 
