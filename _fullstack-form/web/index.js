4442be9f92af3a24b6e4fc6d6bf82d041bb90ea5
Jaime Rios <jaimerios1989@gmail.com>
2022-04-16 11:46:30 -0700

    commented delete func

--- _fullstack-form/web/index.js
+++ _fullstack-form/web/index.js
@@ -39,37 +39,41 @@ const parseData = (data) => {
         <div class="mt-3" id="${i}">
             <h3>${e.todoTitle}</h3>
             <h4>${e.todoDescription}</h4>
-            <button class="delete-section btn btn-xs btn-danger">Delete</button>
+            <!-- <button class="delete-section btn btn-xs btn-danger">Delete</button> -->
         </div>`;
         return template;
     });
 
     content.innerHTML = estosDatos.join('');
 
-    // Delete
-    let deleteButtons = document.querySelectorAll('.delete-section');
-    deleteButtons.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
+    // // Delete
+    // let deleteButtons = document.querySelectorAll('.delete-section');
+    // deleteButtons.forEach((btn) => {
+    //     btn.addEventListener('click', (e) => {
 
-            // const id = btn.parentElement.id
-            // const filteredTodos = data.filter((todo) => {
-            //     return todo.todoTitle !== data[id].todoTitle
-            // })
+    //         const id = btn.parentElement.id
+    //         const filteredTodos = data.filter((todo) => {
+    //             return todo.todoTitle !== data[id].todoTitle
+    //         })
 
-            // filteredTodosJSON = JSON.stringify(filteredTodos);
+    //         filteredTodosJSON = JSON.stringify(filteredTodos);
             
-            // console.log(filteredTodos)
-            // console.log(filteredTodosJSON)
+    //         console.log(filteredTodos)
+    //         console.log(filteredTodosJSON)
 
-            // fetch('http://localhost:666/todos', {
-            //     method: 'DELETE',
-            //     headers: {
-            //         "Content-Type": "application/json",
-            //     },
-            //     body: filteredTodosJSON,
-            // });
-        })
-    });
+    //         fetch('http://localhost:666/todos', {
+    //             method: 'DELETE',
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: filteredTodosJSON,
+    //         });
+    //         getDataTodos();
+    //     })
+    // });
+
+
+
 };
 
 getDataTodos();
