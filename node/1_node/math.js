407e38dfaa2ea41aaab2454638851c2837aa6cac
Jaime Rios <jaimerios1989@gmail.com>
2022-05-01 13:54:34 -0700

    node - export/import functions

--- node/1_node/math.js
+++ node/1_node/math.js
@@ -1,30 +1,17 @@
-exports.add = (a, b) => {
-    return a + b
-}
-exports.minus = (a, b) => {
-    return a - b
-}
-exports.multiply = (a, b) => {
-    return a * b
-}
-exports.divide = (a, b) => {
-    return a / b
-}
-exports.reminder = (a, b) => {
-    return a % b
-}
+const add = (a, b) => {
+    return a + b;
+};
+const minus = (a, b) => {
+    return a - b;
+};
+const multiply = (a, b) => {
+    return a * b;
+};
+const divide = (a, b) => {
+    return a / b;
+};
+const reminder = (a, b) => {
+    return a % b;
+};
 
-
-
-
-// si no le pones el exports.funcName, abajo se exportan
-/*
-    const add = (a, b) => {
-        return a + b
-    }
-    const minus = (a, b) => {
-        return a - b
-    }
-
-    module.exports = { add, minus, multiply, divide, reminder }
-*/
+module.exports = { add, minus, multiply, divide, reminder };
