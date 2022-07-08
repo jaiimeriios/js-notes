9e9c5f179ee8f8f26c17ba81904caeeda712ec56
Jaime Rios <jaimerios1989@gmail.com>
2022-07-07 18:42:44 -0700

    MERN updates server page

--- _mern/server/index.js
+++ _mern/server/index.js
@@ -2,35 +2,19 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 import chalk from 'chalk';
 
 import usersRoutes from './src/routes/users.js';
 import todosRoutes from './src/routes/todos.js';
 
 const app = express();
-const PORT = 666;
+const PORT = process.env.PORT;
+
+// middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-
-const mernTestDB = `mongodb+srv://${dbUser}:${dbPassword}@testdb.g6sqj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-
-mongoose
-    .connect(mernTestDB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log(chalk.bgCyan(' Connected to MongoDB '));
-    });
-
-console.clear();
-
-// HOME
+// ROUTES
 app.get('/', (req, res) => {
     res.send(`
         <h1>Home Page - MongoDB API!</h1>
@@ -41,7 +25,6 @@ app.get('/', (req, res) => {
     `);
 });
 
-// ROUTES
 app.use('/users', usersRoutes);
 app.use('/todos', todosRoutes);
 
@@ -49,9 +32,15 @@ app.all('*', (req, res) => {
     res.send("You've tried reaching a route that doesn't exist.");
 });
 
-app.listen(PORT, () => {
-    console.log(
-        'Server running on port:',
-        chalk.bgGreen(` http://localhost:${PORT} `)
-    );
-});
+// connect to MongoDB using mongoose
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.clear();
+        console.log(chalk.bgCyan(' Connected to MongoDB '));
+        app.listen(PORT, () => {
+            console.log('Server running on:', chalk.bgGreen(` http://localhost:${PORT} `));
+        });
+    })
+    .catch((e) => {
+        console.log(chalk.red(e));
+    });
